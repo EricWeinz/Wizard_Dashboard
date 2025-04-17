cbbcc57510e5cc1fa9ad1b9744bba285c200abeb
EricWeinz <ericweinzierl@gmx.de>
2025-04-18 01:39:17 +0200

    Initial

--- src.py
+++ src.py
@@ -1,4 +1,5 @@
 import customtkinter
+import tkinter
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
